3bae1da81fca841804758a816d0eb76fe5da255d
DarshanPro123 <darshanpro187@gmail.com>
2024-08-18 20:02:28 +0530

    navbar style change in responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -39,19 +39,7 @@ const Navbar = () => {
           </button>
           <ul className={isNavOpen ? "nav-links open" : "nav-links"}>
             {/* close button  */}
-            <button onClick={toggleNav}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width="24"
-                height="24"
-              >
-                <path
-                  fill="currentColor"
-                  d="M18.3 5.71a1 1 0 00-1.42 0L12 10.59 7.12 5.71A1 1 0 105.7 7.12L10.59 12l-4.88 4.88a1 1 0 101.42 1.42L12 13.41l4.88 4.88a1 1 0 001.42-1.42L13.41 12l4.88-4.88a1 1 0 000-1.41z"
-                />
-              </svg>
-            </button>
+            {isNavOpen ? <button onClick={toggleNav}>X</button> : ""}
             <li>
               <NavLink
                 activeClassname="active"
